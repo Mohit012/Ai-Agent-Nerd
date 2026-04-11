c82a2ed250d42b06b55ddfe23a9255467aa0bef7
Mohit012 <maxrawat827@gmail.com>
2026-04-12 01:38:11 +0530

    remove invalud devTools config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  devTools: {
-    enabled: false
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
